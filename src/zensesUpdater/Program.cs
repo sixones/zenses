51e222f108cad381063360b899f40aa399703965
adamlivesley <adamlivesley@c5ca1edb-0f35-0410-b988-7bac8ecc5e99>
2007-07-12 23:00:34 +0000

    updated project files for zensesUpdater
    git-svn-id: https://zenses.googlecode.com/svn/trunk@11 c5ca1edb-0f35-0410-b988-7bac8ecc5e99

--- src/zensesUpdater/Program.cs
+++ src/zensesUpdater/Program.cs
@@ -14,7 +14,7 @@ namespace zensesUpdater
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			//Application.Run(new Form1());
 		}
 	}
 }
