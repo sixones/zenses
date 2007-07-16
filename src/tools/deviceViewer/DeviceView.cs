736b0c0d3560e3c09c6926e1279363bdab4d77d2
adamlivesley <adamlivesley@c5ca1edb-0f35-0410-b988-7bac8ecc5e99>
2007-07-16 23:17:26 +0000

    updated deviceViewer.exe to list tracks
    git-svn-id: https://zenses.googlecode.com/svn/trunk@24 c5ca1edb-0f35-0410-b988-7bac8ecc5e99

--- src/tools/deviceViewer/DeviceView.cs
+++ src/tools/deviceViewer/DeviceView.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 using Zenses.Lib.Constants;
 using Zenses.Lib.Managers;
 using Zenses.Lib.Objects;
+using Zenses.Lib.Collections;
 
 namespace Zenses.Tools.DeviceViewer
 {
@@ -18,6 +20,46 @@ namespace Zenses.Tools.DeviceViewer
 		{
 			InitializeComponent();
 			this.PopulateDeviceListView();
+			Thread thread = new Thread(new ThreadStart(this.PopulateObjectListView));
+			thread.Start();
+		}
+
+		public void PopulateObjectListView()
+		{
+			DeviceManager deviceManager = new DeviceManager();
+			Device device = deviceManager.GetDevices()[0];
+
+			TrackEntries entries = new TrackEntries(device);
+
+			foreach (EntryObject entry in entries)
+			{
+				this.UpdateEntryView(entry);	
+			}
+		}
+
+		public delegate void UpdateEntryViewEvent(EntryObject entry);
+
+		public void UpdateEntryView(EntryObject entry)
+		{
+			if (this.InvokeRequired)
+			{
+				this.BeginInvoke(new UpdateEntryViewEvent(this.UpdateEntryView), entry);
+			}
+			else
+			{
+				string[] subItems = new string[] {
+					entry.Id,
+					entry.Title,
+					entry.Artist,
+					entry.Album,
+					entry.Length.ToString(),
+					entry.Size.ToString()
+				};
+
+				ListViewItem listViewItem = new ListViewItem(subItems);
+
+				this._cDeviceListView.Items.Add(listViewItem);
+			}
 		}
 
 		public void PopulateDeviceListView()
@@ -30,13 +72,13 @@ namespace Zenses.Tools.DeviceViewer
 				foreach (Device device in devices)
 				{
 					string[] subItems = new string[] {
-					device.Id,
-					device.FriendlyName,
-					device.Model,
-					device.Manufacturer,
-					device.SerialNumber,
-					device.BatteryLevel.ToString()
-				};
+						device.Id,
+						device.FriendlyName,
+						device.Model,
+						device.Manufacturer,
+						device.SerialNumber,
+						device.BatteryLevel.ToString()
+					};
 
 					ListViewItem listViewItem = new ListViewItem(subItems);
 
