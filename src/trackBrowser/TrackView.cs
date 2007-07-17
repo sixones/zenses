a5e1afa138bc395b49a3046b46d7acceeab10489
adamlivesley <adamlivesley@c5ca1edb-0f35-0410-b988-7bac8ecc5e99>
2007-07-17 15:39:18 +0000

    created trackBrowser tools application
    git-svn-id: https://zenses.googlecode.com/svn/trunk@28 c5ca1edb-0f35-0410-b988-7bac8ecc5e99

---
+++ src/trackBrowser/TrackView.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using Zenses.Lib.Constants;
+using Zenses.Lib.Managers;
+using Zenses.Lib.Objects;
+using Zenses.Lib.Collections;
+
+namespace Zenses.Tools.TrackBrowser
+{
+	public partial class TrackView : Form
+	{
+		public TrackView()
+		{
+			InitializeComponent();
+
+			Thread thread = new Thread(new ThreadStart(this.PopulateObjectListView));
+			thread.Start();
+		}
+
+		public void PopulateObjectListView()
+		{
+			DeviceManager deviceManager = new DeviceManager();
+			Device device = deviceManager.GetDevices()[0];
+
+			TrackEntries entries = new TrackEntries(device);
+
+			this.UpdateStatus(true);
+
+			foreach (EntryObject entry in entries)
+			{
+				this.UpdateEntryView(entry);
+			}
+
+			this.UpdateStatus(false);
+		}
+
+		public delegate void UpdateStatusEvent(bool visible);
+
+		public void UpdateStatus(bool visible)
+		{
+			if (this.InvokeRequired)
+			{
+				this.BeginInvoke(new UpdateStatusEvent(this.UpdateStatus), visible);
+			}
+			else
+			{
+				this._cStatusLabel.Visible = visible;
+			}
+		}
+
+		public delegate void UpdateEntryViewEvent(EntryObject entry);
+
+		public void UpdateEntryView(EntryObject entry)
+		{
+			if (this.InvokeRequired)
+			{
+				this.BeginInvoke(new UpdateEntryViewEvent(this.UpdateEntryView), entry);
+			}
+			else
+			{
+				string[] subItems = new string[] {
+					entry.Id,
+					entry.Name,
+					entry.Artist,
+					entry.Album,
+					entry.ContentType,
+					entry.PlayCount.ToString()
+				};
+
+				ListViewItem listViewItem = new ListViewItem(subItems);
+
+				this._cTrackListView.Items.Add(listViewItem);
+			}
+		}
+	}
+}
